88cab94c58b0c6ffcd282db381ef392fe4c02b5c
David Machio Renes <davidmachio@MacBook-Pro-de-David.local>
2023-07-28 11:30:58 +0200

    Master Final

--- pages/About/About.js
+++ pages/About/About.js
@@ -15,14 +15,16 @@ const template = () => {
   <img src="https://res.cloudinary.com/dc5wp3crs/image/upload/v1690484067/medidas_iwded1.jpg" alt="Plano de una bicicleta" class="coverabout"/>
   <div class="textabout">
   <h1 class="h1about">Proceso</h1>
-  <p>Desde tomar medidas exactas hasta cortar los tubos de acero con precisión, la elección del acabado y el montaje final, cada etapa del proceso de construcción de una bicicleta se fusiona para dar vida a una obra maestra sobre dos ruedas.
+  <p>Comienza con la selección y medición cuidadosa de los tubos de acero. Posteriormente, son cortados de manera precisa siguiendo el diseño que se ha escogido previamente, asegurando así la base para una bicicleta única.
+
+  Luego, se lleva a cabo el proceso de soldado de los tubos. Aquí, la habilidad del artesano juega un papel crucial para garantizar la integridad y seguridad del cuadro. La selección del diseño es un paso crucial, ya que determina la funcionalidad, el estilo y la personalidad de la bicicleta, considerando factores como el tipo de ciclismo para el cual será utilizada.
+</p>
   </div>
   </div>
   <div class="divabout">
   <div class="textabout">
   <h1 class="h1about">Entrega</h1>
-  <p>Mi nombre es Pablo y me dedico apasionadamente a construir bicicletas de acero a mano.
-  Mi arte reside en la combinación de la tradición artesanal con la precisión técnica, creando dos ruedas que son obras de arte funcionales, listas para conquistar caminos y senderos.
+  <p>Finalmente, el montaje de la bicicleta se lleva a cabo con cuidado y precisión, incorporando componentes como el sistema de frenado, la transmisión y los componentes de la dirección, asegurando que cada detalle esté ajustado correctamente para garantizar un rendimiento óptimo. El resultado final es una bicicleta en acero hecha a mano, una obra de arte funcional que refleja la pasión y dedicación de los artesanos involucrados en su construcción.</p>
   </div>
   <img src="https://res.cloudinary.com/dc5wp3crs/image/upload/v1690484067/bikecover_mxtedi.jpg" alt="Bicleta montada" class="coverabout"/>
   </div>
--- pages/Trabajos/Trabajos.js
+++ pages/Trabajos/Trabajos.js
@@ -4,15 +4,15 @@ import bikes from "../../data/Bikes";
 const template = () => {
   return `
   <div class="video">
-        <video width="100%" height=auto src="../videos/videohumo.mp4" autoplay controls>Tu navegador no admite formato mp4 video</video>
-        <div class="enlace">
-    
+        <video width="100%" height=auto src="../videos/videohumo.mp4" muted autoplay controls>Tu navegador no admite formato mp4 video</video>
+  
+
+  <div class="enlace">
       <a class="watch" target="_blank" href="https://youtube.com/shorts/gxtyY8zi1r0?feature=share">▶︎ watch</a>
-     </div>
-    </div>
+  </div>
+  </div>
     <h2 class="trabajos">Bikes</h2>
     <ul id="trabajos-container"></ul>
-    <a href="#null" id="home-link"><img class="logo" src="./icons/bbloqueletras.png"/></a>
     `;
 };
 
